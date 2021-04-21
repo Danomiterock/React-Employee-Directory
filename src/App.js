13240ea81ccadb0199dc49841375bd53117fdeb7
Daniel Lucas <danomiterock@yahoo.com>
2021-04-21 11:13:34 -0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
---
+++ src/components/employees/EmployeeTable.js
@@ -0,0 +1,3 @@
+import React, { useState, useEffect } from "react";
+import { Table, Col, Row } from "react-bootstrap";
+import SearchBar from "../search/SearchBar";
