5662874d563f7971895962e8a50faaf2dff7ed5a
Daniel Lucas <danomiterock@yahoo.com>
2021-04-22 09:36:05 -0700

    Update navbar with template from bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,12 @@ import "../styles/Navbar.css";
 
 function Navbar() {
     return (
-      <nav className="navbar">
-        <a href="/">Welcome</a>
+      <nav class="navbar navbar-light bg-light justify-content-between">
+        <a class="navbar-brand">Navbar</a>
+          <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
       </nav>
     );
   }
