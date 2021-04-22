d259491d2f48ec3e218e343f8741a23939fe1a93
Daniel Lucas <danomiterock@yahoo.com>
2021-04-22 16:55:08 -0700

    Update the Navbar heading

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import "../styles/Navbar.css";
 function Navbar() {
     return (
       <nav class="navbar navbar-light bg-light justify-content-between">
-        <a class="navbar-brand">Navbar</a>
+        <a class="navbar-brand">Employee Directory</a>
           <form class="form-inline">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
