e4c2b56cb427c3846ceb30f5fe47a65605f41e1b
Daniel Lucas <danomiterock@yahoo.com>
2021-04-23 14:24:53 -0700

    uodate search form

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { Container, Form, Jumbotron, Button, FormControl } from "react-bootstrap";
+import { Container, Form, Jumbotron, FormControl } from "react-bootstrap";
 import "../styles/Jumbo.css";
 
 // By importing the Navbar.css file, it is added to the DOM whenever this component loads
 
-function Jumbo() {
+function Jumbo(props) {
   return (
     <Jumbotron fluid>
       <Container className="d-flex justify-content-between px-5">
         <h1 className="pl-4">Employee Directory </h1>
         <Form inline>
-          <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
-          <Button type="submit">Submit</Button>
+          <FormControl
+            onChange={props.handleInputChange}
+            name="search"
+            type="text"
+            placeholder="Search"
+            className="form-control"
+            id="search"
+          />
         </Form>
       </Container>
     </Jumbotron>
